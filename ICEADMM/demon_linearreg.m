8bc1482adc544ba4c2096d3be8097872d919f75c
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2022-03-25 11:45:32 +0000

    Update demon_linearreg.m

--- ICEADMM/demon_linearreg.m
+++ ICEADMM/demon_linearreg.m
@@ -1,17 +1,17 @@
 clc; clear; close all;
 addpath(genpath(pwd));
+ 
+m     = 32;
+n     = 300;  
+k0    = 5;
 
-k0    = 10;  
-n     = 100;
-m     = 120;
-di    = randi([50 150],1,m); 
+di    = randi([50 150],1,m);
 [A,b] = GenerateData(m,n,di,1/3,1/3); 
 
-pars.r0 = 0.1;                              % incease this value if you find the solver diverges
-out0    = ICEADMMLin(di,n,A,b,k0,0,pars)    % CEADMM
+pars1.r0 = 0.1;                                    % incease this value if you find the solver diverges
+out1     = ICEADMM4LinearReg(di,n,A,b,k0,0,pars1)  % Exact   ADMM
+plotobj(out1.iter,out1.objy,out1.objX,k0,'CEADMM');  
 
-pars.r0 = 0.2;                              % incease this value if you find the solver diverges
-out1    = ICEADMMLin(di,n,A,b,k0,1,pars)    % ICEADMM
-
-plotobj(out0.iter,out0.objy,out0.objX,'CEADMM')
-plotobj(out1.iter,out1.objy,out1.objX,'ICEADMM')
+pars2.r0 = 0.1;                                    % incease this value if you find the solver diverges
+out2     = ICEADMM4LinearReg(di,n,A,b,k0,1,pars2)  % Inexact ADMM
+plotobj(out2.iter,out2.objy,out2.objX,k0,'ICEADMM')
